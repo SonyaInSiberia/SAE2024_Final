6b03f86b3b082f62cc9b9e02b22c690c31a432f1
DavidJones10 <102177843+DavidJones10@users.noreply.github.com>
2024-03-14 13:21:23 -0400

    Changed sampler voice to fill ringbuffer outside of struct. Now you pass in the same buffer for each voice in the process function.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
 mod ring_buffer;
+use ring_buffer::RingBuffer;
 mod sample_voice;
-use hound::WavSpec;
+use hound::{WavReader, WavSpec, SampleFormat};
+use std::io::BufReader;
+use std::fs::File;
 use sample_voice::SampleVoice;
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let mut reader = hound::WavReader::open(&args[1]).unwrap();
     let wav_size = reader.len();
-    let fill_buffer = vec![0.0;wav_size as usize];
+    let channels = reader.spec().channels as usize;
+    let mut wav_as_ring = RingBuffer::<f32>::new(wav_size as usize);
+    fill_buffer(&mut wav_as_ring, &mut reader);
     let base_note = 60;
-    let mut sampVoice = SampleVoice::new(&mut reader, base_note);
-    /* let mut voices: Vec<SampleVoice> = (0..5)
-    .map(|_| SampleVoice::new(&mut reader, base_note))
-    .collect(); */
+    //let mut sampVoice = SampleVoice::new( channels,base_note);
+    let mut voices: Vec<SampleVoice> = (0..5)
+    .map(|_| SampleVoice::new(channels, base_note))
+    .collect();
 
     let newSpec = hound::WavSpec {
         channels: reader.spec().channels,
@@ -28,21 +33,70 @@ fn main() {
         }
     };
 
-    /* for (i,voice) in voices.iter_mut().enumerate(){
-        voice.set_note(base_note + (i as u8 * 3));
-    } */
-    sampVoice.set_note(midi_note);
+    for (i,voice) in voices.iter_mut().enumerate(){
+        voice.set_note(base_note + (i as u8 * 5));
+    }
+    //sampVoice.set_note(midi_note);
     
     let mut writer = hound::WavWriter::create(&args[2], newSpec).unwrap();
     for i in (0..wav_size){
-        /* let mut outSample = 0.0;
+        let mut outSample = 0.0;
         for voice in voices.iter_mut(){
-            outSample += voice.process();
-        } */
-        let outSample = sampVoice.process();
+            outSample += voice.process(&mut wav_as_ring);
+        }
+        //let outSample = sampVoice.process(&mut wav_as_ring);
         writer.write_sample(outSample);
     }
     writer.finalize().unwrap();
-    println!("This is your sample {} semitones apart", midi_note as i8 -base_note as i8);
-    //println!("This is your sample... but with chords now!!");
+    //println!("This is your sample {} semitones apart", midi_note as i8 -base_note as i8);
+    println!("This is your sample... but with chords now!!");
 }
+
+
+/// Fills the internal buffers with the samples from the wav file
+/// 
+/// Takes into account the number of channels, bits per sample, and float vs int values.  
+fn fill_buffer(buffer: &mut RingBuffer<f32>, reader: &mut WavReader<BufReader<File>>) {
+    let sample_format = reader.spec().sample_format;
+    let num_channels = reader.spec().channels as usize;
+    let sample_rate = reader.spec().sample_rate as f32;
+    let length = reader.len();
+    // Determine the conversion factor based on sample format
+    let conversion_factor = match sample_format {
+        SampleFormat::Float => 1.0, // No conversion needed
+        SampleFormat::Int => {
+            match reader.spec().bits_per_sample {
+                8 => 1.0 / (i8::MAX as f32),
+                16 => 1.0 / (i16::MAX as f32),
+                24 => 1.0 / (8388608 as f32), 
+                _ => panic!("Unsupported bit depth"),
+            }
+        }
+    };
+    match sample_format{
+        SampleFormat::Float => {
+            let mut samples = reader.samples::<f32>();
+            for _ in 0..(length) {
+                if let Some(sample) = samples.next() {
+                    if let Ok(sample_value) = sample {
+                        let sample_float = sample_value * conversion_factor;
+                         buffer.push(sample_float);
+                    }
+                }
+            }
+        }, 
+        SampleFormat::Int => {
+            let mut samples = reader.samples::<i32>();
+            for _ in 0..(length) {
+                
+                if let Some(sample) = samples.next() {
+                    if let Ok(sample_value) = sample {
+                        let sample_float = (sample_value as f32) * conversion_factor;
+                        buffer.push(sample_float);
+                    }
+                }
+            }
+        }
+    }
+}
+
--- src/sample_voice.rs
+++ src/sample_voice.rs
@@ -1,119 +1,52 @@
 use crate::ring_buffer;
 use ring_buffer::RingBuffer;
-use hound::{WavReader, WavSpec, SampleFormat};
-use std::io::BufReader;
-use std::fs::File;
+
 pub struct SampleVoice{
-    spec: WavSpec,
-    buffers: Vec<RingBuffer::<f32>>,
-    phase_offsets: Vec<f32>,
+    phase_offset: f32,
     phase_step: f32,
     offset_midi: i8,
     bass_midi: u8,
-    channel_id: usize,
+    num_channels: usize,
 }
 
 impl SampleVoice{
     /// Takes in a WaveReader with an open file and the midi note that you want to place your sample at
-    pub fn new(reader: &mut WavReader<BufReader<File>>, base_midi_: u8)->Self{
+    pub fn new(num_channesls_: usize, base_midi_: u8)->Self{
         // I want this line to split the vector by channels, so like this [left channel][right channel]
         // This way i can fill the buffers with their corresponding samples
-        let spec = reader.spec();//(reader.len()/spec.channels as u32)as usize)
-        let buffs_: Vec<RingBuffer<f32>> = (0..spec.channels as usize)
-        .map(|_| RingBuffer::<f32>::new((reader.len()/spec.channels as u32)as usize))
-        .collect();
-        let offsets = vec![0.0;spec.channels as usize];
         
         let mut voice = SampleVoice{
-            spec: spec,
-            buffers: buffs_,
-            phase_offsets: offsets,
-            phase_step: 0.0,
+            phase_offset: 0.0,
+            phase_step: 1.0,
             offset_midi: 0,
             bass_midi: base_midi_,
-            channel_id: 0,
+            num_channels: num_channesls_,
         };
-        voice.fill_buffers(reader);
+        //voice.fill_buffers(reader);
 
         voice
     }
     ///Reads from the internal buffer a the rate set by note_offset
     /// Uses the get_frac function in the ring_buffer, which returns the sample
     /// at a fractional index
-    pub fn process(&mut self)->f32{
-        self.channel_id =  self.channel_id % self.spec.channels as usize;
-        let sample = self.buffers[self.channel_id].get_frac(self.phase_offsets[self.channel_id]);
-        self.phase_offsets[self.channel_id] += self.phase_step;
+    pub fn process(&mut self, buffer: &mut RingBuffer<f32>)->f32{
+        let sample = buffer.get_frac(self.phase_offset);
+        self.phase_offset += self.phase_step;
         
-        if self.phase_offsets[self.channel_id] >= self.buffers[0].capacity() as f32 {
+        if self.phase_offset >= buffer.capacity() as f32 {
             self.phase_step = 0.0;
             //self.phase_offsets[self.channel_id] -= self.buffers[0].capacity() as f32;
         }
 
         sample
     }
-
     ///Sets the midi note for the output
     /// 
     /// Is in reference to the base midi note
     pub fn set_note(&mut self, note: u8){
         self.offset_midi = note as i8 - self.bass_midi as i8;
         let offset = iclamp((self.offset_midi)as i32,-127,127);
-        self.phase_step = 2.0_f32.powf(offset as f32 / 12.0) / self.spec.channels as f32;
-    }
-    /// Fills the internal buffers with the samples from the wav file
-    /// 
-    /// Takes into account the number of channels, bits per sample, and float vs int values.  
-    fn fill_buffers(&mut self, reader: &mut WavReader<BufReader<File>>) {
-        let sample_format = reader.spec().sample_format;
-        let num_channels = self.spec.channels as usize;
-        let sample_rate = self.spec.sample_rate as f32;
-        let length = reader.len();
-
-        // Determine the conversion factor based on sample format
-        let conversion_factor = match sample_format {
-            SampleFormat::Float => 1.0, // No conversion needed
-            SampleFormat::Int => {
-                match self.spec.bits_per_sample {
-                    8 => 1.0 / (i8::MAX as f32),
-                    16 => 1.0 / (i16::MAX as f32),
-                    24 => 1.0 / (8388608 as f32), 
-                    _ => panic!("Unsupported bit depth"),
-                }
-            }
-        };
-
-        match sample_format{
-            SampleFormat::Float => {
-                let mut samples = reader.samples::<f32>();
-                for _ in 0..(length / num_channels as u32) {
-                    for ch in 0..num_channels {
-                        if let Some(sample) = samples.next() {
-                            if let Ok(sample_value) = sample {
-                                let sample_float = sample_value * conversion_factor;
-                                self.buffers[ch].push(sample_float);
-                            }
-                        }
-                    }
-                }
-            }, 
-            SampleFormat::Int => {
-                let mut samples = reader.samples::<i32>();
-                for _ in 0..(length / num_channels as u32) {
-                    for ch in 0..num_channels {
-                        if let Some(sample) = samples.next() {
-                            if let Ok(sample_value) = sample {
-                                let sample_float = (sample_value as f32) * conversion_factor;
-                                self.buffers[ch].push(sample_float);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        // Calculates phase step based on sample rate
-        self.phase_step =  2.0_f32.powf(self.offset_midi as f32 / 12.0) / num_channels as f32 ;
+        self.phase_step = 2.0_f32.powf(offset as f32 / 12.0);
     }
 }
 
